2b76e35b8785580bab4edd7987b5fa9b58c3cf24
LamaWaffle <matty.eberhard@gmail.com>
2022-04-15 10:59:42 +1000

    added a line at the end of the file.

--- Day3/Pizza-Order.py
+++ Day3/Pizza-Order.py
@@ -5,6 +5,8 @@ size = input("What size pizza do you want? S, M, or L ")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
 
+bill = 0
+
 if size == "S":
     bill = 15
     if add_pepperoni == "Y":
@@ -25,5 +27,4 @@ if size == "L":
         bill = bill + 3
     if extra_cheese == "Y":
         bill += 1
-
 print(f"Your final bill is: ${bill}")
