2c80237a7ee5f6d70c0a4c7527063408d9828d95
LamaWaffle <matty.eberhard@gmail.com>
2022-04-16 23:35:33 +1000

    new game

--- Day3/Treasure-Island.py
+++ Day3/Treasure-Island.py
@@ -1,4 +1,8 @@
 
+from platform import python_branch
+from webbrowser import open_new_tab
+
+
 print(""" _                                                           
 88888b.  .d88b.  .d88b.  8888b. .d8888b 888  888.d8888b  
 888 "88bd8P  Y8bd88P"88b    "88b88K     888  88888K      
@@ -12,26 +16,69 @@ print(""" _
 
 username = input("What is your name?\n")
 
-print("Welcome " + username )
+print("Welcome, " + username )
 
 print("You've been trapped in this simulation.. Your goal is to escape..Do you understand?\n")
-intro = input("Y or N\n").lower()
 
-choice1 = input("Which way do you turn, left or right?\n").lower()
-
-if choice1 == "left":
-    choice2 = input("You come across a river, do you wait or swim?\n").lower()
-    if choice2 == "wait":
-        choice3 = input("You see three doors, a blue door, a yellow door and a red door, which door do you go through?\n").lower()
-        if choice3 == "yellow":
-            print("YOU WIN!")
-        elif choice3 == "red":
-            print("You are burned by fire, game over.")
-        elif choice3 == "blue":
-            print("You are eaten by beasts, game over.")
-        else:
-            print("You choose a door that doesnt exist, you lose.")
+intro = input("Y or N ?\n")
+if intro.lower() == "y":
+    print("You are currently in a dark room, you feel confused.\n You have no memory of how you got here..\n")
+    light_switch = input("You can see a light in front of you, do you turn it on?\n>")
+    if light_switch == "y":
+        print("Light turns on")
+        
     else:
-        print("You get eaten by sharks")
+        print("You feel too weak to do anything, you try to gather the energy to get up, but you break a hip while doing so.. Resulting in your immidiate death..\n")
+        exit()
+        #Import its own file. Add exit() program if NO.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 else:
-    print("You die, bad luck.")
+    intro.lower() == "n"
+    exit()
+
+
+
+
+
+
+# hungry = input("Are you hungry? True or false \n")
+
+# if hungry.lower() == "true":
+#     print("Feed me!")
+# else:
+#     print("I'm not hungry.")
+
+
+
+
+# choice1 = input("Which way do you turn, left or right?\n").lower()
+
+# if choice1 == "left":
+#     choice2 = input("You come across a river, do you wait or swim?\n").lower()
+#     if choice2 == "wait":
+#         choice3 = input("You see three doors, a blue door, a yellow door and a red door, which door do you go through?\n").lower()
+#         if choice3 == "yellow":
+#             print("YOU WIN!")
+#         elif choice3 == "red":
+#             print("You are burned by fire, game over.")
+#         elif choice3 == "blue":
+#             print("You are eaten by beasts, game over.")
+#         else:
+#             print("You choose a door that doesnt exist, you lose.")
+#     else:
+#         print("You get eaten by sharks")
+# else:
+#     print("You die, bad luck.")
