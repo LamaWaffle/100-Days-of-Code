ce390de59862d7bbe149c080e780bfde41018433
LamaWaffle <matty.eberhard@gmail.com>
2022-04-17 10:10:28 +1000

    added list file

--- Day4/Heads-Tails.py
+++ Day4/Heads-Tails.py
@@ -2,15 +2,15 @@ import random
 # numbers = random.randint(0, 100)
 # random_float = random.random()
 
-# Option 1
-flip = random.randint(0, 1)
-if flip == 0:
-    print("Tails")
-else:
-    print("Heads")
+# # # Option 1
+# flip = random.randint(0, 1)
+# if flip == 0:
+#     print("Tails")
+# else:
+#     print("Heads")
 
-# Option 2
-flip = random.randint(0, 1) if flip == 1: print('Heads') else: print("Tails")
+# # Option 2
+# flip = random.randint(0, 1) if flip == 1: print('Heads') else: print("Tails")
 
 # Option 3
-print("Heads" if random.randint(0, 1) == 0 else "Tails")
+print("Heads" if random.randint(0, 1) == 1 else "Tails")
