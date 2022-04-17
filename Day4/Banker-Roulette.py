20c3dfc0d3ca43fc20d19a732d8453fd880bdcad
LamaWaffle <matty.eberhard@gmail.com>
2022-04-17 11:09:19 +1000

    fml

---
+++ Day4/Banker-Roulette.py
@@ -0,0 +1,14 @@
+import random
+# 
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")    # List of names.
+# 
+
+
+number_of_names = len(names) -1   # Gives value of how many items from the list.
+
+index_number = random.randint(0,number_of_names)
+
+luckyone = names[index_number]
+
+print(f"{luckyone} is going to buy the meal today!")
