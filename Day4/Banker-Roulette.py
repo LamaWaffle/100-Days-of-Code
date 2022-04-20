958cbd3780ab380f0b4c05c06698bc33201ad7ce
LamaWaffle <matty.eberhard@gmail.com>
2022-04-20 20:57:28 +1000

    added some new files, removed others. Also finally got lists, omg

--- Day4/Banker-Roulette.py
+++ Day4/Banker-Roulette.py
@@ -4,13 +4,15 @@ names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")    # List of names.
 # 
 
-
 number_of_names = len(names) -1   # Gives value of how many items from the list.
+random_index = random.randint(0, number_of_names)
 
-index_number = random.randint(0,number_of_names)
+random_name = names[random_index]
+print(f"{random_name} is going to buy the meal today!")
 
-luckyone = names[index_number]
 
-print(f"{luckyone} is going to buy the meal today!")
+# OR a shorter way to do it is..
+random_index = random.randint(0, len(names) - 1)
 
-# Come back to this course, number 44 from 100 days of code.
+random_name = names[random_index]
+print(f"{random_name} is going to buy the meal today!")
--- Day4/Dirty-Dozen.py
+++
@@ -1,4 +0,0 @@
-
-# dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
-
-dirty_dozen = [fruits, vegetables]
