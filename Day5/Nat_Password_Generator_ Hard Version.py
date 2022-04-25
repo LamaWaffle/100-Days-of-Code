f4a0f1e0e3d6664c6b38455ec3e8c2fa0fee569c
kamonnatpongriyawattana <pnut1117@gmail.com>
2022-04-25 23:58:43 +1000

    add some explainaion for code

--- Day5/Nat_Password_Generator_ Hard Version.py
+++ Day5/Nat_Password_Generator_ Hard Version.py
@@ -12,19 +12,20 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 password=""
-all_lists = [letters, numbers, symbols]
+all_lists = [letters, numbers, symbols] #list for randomising which type of character we will pick.
 
-letters_quota = nr_letters
+letters_quota = nr_letters  # I don't need to create extra variable but I would like to change the name of variable for my own understanding.
 symbols_quota = nr_symbols
 numbers_quota = nr_numbers
 
 
-while numbers_quota != 0 or letters_quota != 0 or symbols_quota != 0:
-  rand1 = random.choice(all_lists)
+while numbers_quota != 0 or letters_quota != 0 or symbols_quota != 0: #as long as my expression is true, the statement below will keep looping.
+  rand1 = random.choice(all_lists) #pick if it should be letter or number or symbol
+#If the randomiser decides it will be letters, it will check the "if" condition below.
 
-  if rand1 == letters and letters_quota != 0:
+  if rand1 == letters and letters_quota != 0: #If it picks letters and letter quoter in our password is still available >> the statement below will be executed
     password += random.choice(letters)
-    letters_quota = letters_quota - 1
+    letters_quota = letters_quota - 1 #After one letter quota is used, this will updated the letter quota that we have already use 1 letter.
 
   elif rand1 == numbers and numbers_quota != 0:
     password += random.choice(numbers)
@@ -34,4 +35,4 @@ while numbers_quota != 0 or letters_quota != 0 or symbols_quota != 0:
     password += random.choice(symbols)
     symbols_quota = symbols_quota - 1
 
-print(password)
+print(f"Your password will be \"{password}\".\nWarning: Please take note of this! It's irretrievable after this is reset.")
